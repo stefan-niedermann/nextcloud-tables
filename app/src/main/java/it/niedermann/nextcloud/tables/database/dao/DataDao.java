3a012dfc1ebd54fa3d8c546866cd18e0d6ae3572
Stefan Niedermann <info@niedermann.it>
2023-05-08 10:38:53 +0200

    fix: Use LEFT JOIN instead of INNER JOIN to also display rows without any values
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/tables/database/dao/DataDao.java
+++ app/src/main/java/it/niedermann/nextcloud/tables/database/dao/DataDao.java
@@ -12,15 +12,15 @@ import it.niedermann.nextcloud.tables.database.entity.Data;
 public interface DataDao extends GenericDao<Data> {
 
     @Query("SELECT d.* FROM Data d " +
-            "INNER JOIN `Row` r ON d.rowId = r.id " +
-            "INNER JOIN `Column` c ON d.columnId = c.id " +
+            "LEFT JOIN `Row` r ON d.rowId = r.id " +
+            "LEFT JOIN `Column` c ON d.columnId = c.id " +
             "WHERE d.rowId = :rowId " +
             "ORDER BY r.remoteId, c.remoteId")
     Data[] getDataForRow(long rowId);
 
     @Query("SELECT d.* FROM Data d " +
-            "INNER JOIN `Row` r ON d.rowId = r.id " +
-            "INNER JOIN `Column` c ON d.columnId = c.id " +
+            "LEFT JOIN `Row` r ON d.rowId = r.id " +
+            "LEFT JOIN `Column` c ON d.columnId = c.id " +
             "WHERE r.tableId = :tableId " +
             "AND c.tableId = :tableId " +
             "ORDER BY r.remoteId, c.orderWeight")
