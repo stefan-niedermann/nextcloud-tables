e87271be18dd155c8a928eb0b75ed06b1d314bdb
Stefan Niedermann <info@niedermann.it>
2024-03-02 10:10:33 +0100

    fix: Fix logging of Tables server app version
    Refs: #36
    
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/tables/ui/exception/ExceptionDialogFragment.java
+++ app/src/main/java/it/niedermann/nextcloud/tables/ui/exception/ExceptionDialogFragment.java
@@ -52,7 +52,7 @@ public class ExceptionDialogFragment extends AppCompatDialogFragment {
 
         final var adapter = new TipsAdapter(actionIntent -> requireActivity().startActivity(actionIntent));
 
-        final String debugInfos = ExceptionUtil.getDebugInfos(requireContext(), throwable, BuildConfig.FLAVOR, account == null ? null : String.valueOf(account.getNextcloudVersion()));
+        final String debugInfos = ExceptionUtil.getDebugInfos(requireContext(), throwable, BuildConfig.FLAVOR, account == null ? null : String.valueOf(account.getTablesVersion()));
 
         binding.tips.setAdapter(adapter);
         binding.stacktrace.setText(debugInfos);
