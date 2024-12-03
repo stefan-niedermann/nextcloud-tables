2865ceb28fd1fd176ff230c204f2487c466eb1a2
Stefan Niedermann <info@niedermann.it>
2024-12-03 18:33:39 +0100

    chore: Define preconfigured SharedExecutors
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

---
+++ shared/src/main/java/it/niedermann/nextcloud/tables/shared/config/SharedExecutors.java
@@ -0,0 +1,14 @@
+package it.niedermann.nextcloud.tables.shared.config;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+
+public final class SharedExecutors {
+
+    public static final ExecutorService CPU = ForkJoinPool.commonPool();
+    public static final ExecutorService IO_NET = Executors.newFixedThreadPool(50);
+    public static final ExecutorService IO_DB_INPUT = Executors.newSingleThreadExecutor();
+    public static final ExecutorService IO_DB_OUTPUT = Executors.newCachedThreadPool();
+
+}
