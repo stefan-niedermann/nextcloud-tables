f8aafbc51b39b4f34c160b1ece0f102f404bc450
Stefan Niedermann <info@niedermann.it>
2025-01-12 14:58:40 +0100

    chore(dao): Implement generic upsert method
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- database/src/main/java/it/niedermann/nextcloud/tables/database/dao/GenericDao.java
+++ database/src/main/java/it/niedermann/nextcloud/tables/database/dao/GenericDao.java
@@ -3,6 +3,7 @@ package it.niedermann.nextcloud.tables.database.dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Update;
+import androidx.room.Upsert;
 
 import it.niedermann.nextcloud.tables.database.entity.AbstractEntity;
 
@@ -18,6 +19,10 @@ public interface GenericDao<T extends AbstractEntity> {
     @SuppressWarnings("unchecked")
     @Update
     void update(T... entity);
+
+    /** @noinspection unchecked */
+    @Upsert
+    void upsert(T... entity);
 
     @SuppressWarnings("unchecked")
     @Delete
