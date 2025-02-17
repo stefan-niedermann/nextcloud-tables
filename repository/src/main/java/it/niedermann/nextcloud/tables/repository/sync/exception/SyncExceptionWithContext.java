6ceb7c79e126331b92815d89003fdd6f12782c33
Stefan Niedermann <info@niedermann.it>
2025-02-17 11:49:12 +0100

    fix: Make SyncExceptionWithContext implement Serializable to avoid crash
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- repository/src/main/java/it/niedermann/nextcloud/tables/repository/sync/exception/SyncExceptionWithContext.java
+++ repository/src/main/java/it/niedermann/nextcloud/tables/repository/sync/exception/SyncExceptionWithContext.java
@@ -3,11 +3,12 @@ package it.niedermann.nextcloud.tables.repository.sync.exception;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.CompletionException;
 
-public abstract class SyncExceptionWithContext extends CompletionException {
+public abstract class SyncExceptionWithContext extends CompletionException implements Serializable {
 
     public SyncExceptionWithContext(@Nullable Throwable cause) {
         super(cause);
@@ -35,16 +36,16 @@ public abstract class SyncExceptionWithContext extends CompletionException {
     }
 
     @NonNull
-    public abstract Map<String, Object> getAttributes();
+    public abstract Map<String, Serializable> getAttributes();
 
     /// @noinspection UnusedReturnValue
     /// because it is thrown
     @NonNull
-    public abstract SyncExceptionWithContext provide(@Nullable Object value);
+    public abstract SyncExceptionWithContext provide(@Nullable Serializable value);
 
-    /// @see #provide(Object)
+    /// @see #provide(Serializable)
     @NonNull
-    public final SyncExceptionWithContext provide(@Nullable Object... values) {
+    public final SyncExceptionWithContext provide(@Nullable Serializable... values) {
         if (values != null) {
             Arrays.stream(values).forEach(this::provide);
         }
