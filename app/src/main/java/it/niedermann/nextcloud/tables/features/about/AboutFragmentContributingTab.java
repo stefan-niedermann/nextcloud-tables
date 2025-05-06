63dd6382ed88eb6592975bf47c2a94e5b4d33fd5
Stefan Niedermann <info@niedermann.it>
2025-05-06 14:19:33 +0200

    chore(databinding): Prepare about tab fragments to databinding
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/tables/features/about/AboutFragmentContributingTab.java
+++ app/src/main/java/it/niedermann/nextcloud/tables/features/about/AboutFragmentContributingTab.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import it.niedermann.nextcloud.tables.R;
@@ -17,7 +18,9 @@ public class AboutFragmentContributingTab extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentAboutContributionTabBinding.inflate(inflater, container, false);
+        binding = DataBindingUtil.inflate(LayoutInflater.from(requireContext()), R.layout.fragment_about_contribution_tab, container, false);
+
+        binding.setLifecycleOwner(getViewLifecycleOwner());
         binding.aboutSource.setText(getString(R.string.about_source, getString(R.string.url_source)));
         binding.aboutIssues.setText(getString(R.string.about_issues, getString(R.string.url_issues)));
         binding.aboutTranslate.setText(getString(R.string.about_translate, getString(R.string.url_translations)));
--- app/src/main/java/it/niedermann/nextcloud/tables/features/about/AboutFragmentLicenseTab.java
+++ app/src/main/java/it/niedermann/nextcloud/tables/features/about/AboutFragmentLicenseTab.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import it.niedermann.nextcloud.tables.R;
@@ -21,7 +22,9 @@ public class AboutFragmentLicenseTab extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentAboutLicenseTabBinding.inflate(inflater, container, false);
+        binding = DataBindingUtil.inflate(LayoutInflater.from(requireContext()), R.layout.fragment_about_license_tab, container, false);
+
+        binding.setLifecycleOwner(getViewLifecycleOwner());
         setTextWithURL(binding.aboutIconsDisclaimerAppIcon, getResources(), R.string.about_icons_disclaimer_app_icon, R.string.about_app_icon_author_link_label, R.string.url_about_icon_author);
         setTextWithURL(binding.aboutIconsDisclaimerMdiIcons, getResources(), R.string.about_icons_disclaimer_mdi_icons, R.string.about_icons_disclaimer_mdi, R.string.url_about_icons_disclaimer_mdi);
         binding.aboutAppLicenseButton.setOnClickListener((v) -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_license)))));
