8baf72f2418ce9be3e442673487be0b804f04ea7
Stefan Niedermann <info@niedermann.it>
2025-05-23 09:20:33 +0200

    fix(database): Fix upgrade migration issues
    Refs: #189
    
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- database/src/main/java/it/niedermann/nextcloud/tables/database/migration/Migration_4_5.java
+++ database/src/main/java/it/niedermann/nextcloud/tables/database/migration/Migration_4_5.java
@@ -2,6 +2,6 @@ package it.niedermann.nextcloud.tables.database.migration;
 
 import androidx.room.migration.AutoMigrationSpec;
 
-///  Adds `archived` and `favorite` columns to `Column` entity
+///  Adds `archived` and `favorite` columns to `Table` entity
 public class Migration_4_5 implements AutoMigrationSpec {
 }
--- database/src/main/java/it/niedermann/nextcloud/tables/database/migration/Migration_5_6.java
+++ database/src/main/java/it/niedermann/nextcloud/tables/database/migration/Migration_5_6.java
@@ -1,10 +1,15 @@
 package it.niedermann.nextcloud.tables.database.migration;
 
+import android.database.sqlite.SQLiteException;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 import androidx.room.migration.Migration;
 import androidx.sqlite.SQLite;
 import androidx.sqlite.SQLiteConnection;
 
+/// @see <a href="https://github.com/stefan-niedermann/nextcloud-tables/issues/189">Issue #189</a>
+///
 /// - Adds `usergroupSelectTeams` flag to `Column` entity
 ///
 /// The following steps are "breaking changes" because users will lose local not yet synchronized changes
@@ -12,22 +17,29 @@ import androidx.sqlite.SQLiteConnection;
 /// - Changes the relation of `DefaultValueUserGroupCrossRef` from `Data` to `Column`
 public class Migration_5_6 extends Migration {
 
+    private static final String TAG = Migration_5_6.class.getSimpleName();
+
     public Migration_5_6() {
         super(5, 6);
     }
 
     @Override
     public void migrate(@NonNull final SQLiteConnection connection) {
-        addUserGroupSelectTeamsToColumn(connection);
+        addUserGroupSelectTeamsToColumnIfNotExists(connection);
         replaceDefaultValueSelectionOptionCrossRefForeignKey(connection);
         replaceDefaultValueUserGroupCrossRefForeignKey(connection);
     }
 
-    private void addUserGroupSelectTeamsToColumn(@NonNull final SQLiteConnection connection) {
-        SQLite.execSQL(connection, """
-                ALTER TABLE `Column`
-                ADD `usergroupSelectTeams` INTEGER NOT NULL DEFAULT 0
-                """);
+    private void addUserGroupSelectTeamsToColumnIfNotExists(@NonNull final SQLiteConnection connection) {
+        // [Inspiration](https://stackoverflow.com/a/43524278)
+        try {
+            SQLite.execSQL(connection, """
+                    ALTER TABLE `Column`
+                    ADD `usergroupSelectTeams` INTEGER NOT NULL DEFAULT 0
+                    """);
+        } catch (SQLiteException ex) {
+            Log.w(TAG, "Failed to add `usergroupSelectTeams` to `Column`: " + ex.getMessage());
+        }
     }
 
     private void replaceDefaultValueSelectionOptionCrossRefForeignKey(@NonNull final SQLiteConnection connection) {
